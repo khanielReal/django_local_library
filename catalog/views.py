d98e83e3f61aa7f8455b1b6cae8c16e47fdfb1a3
Haniel Kamau <kamauhaniel@gmail.com>
2025-06-29 00:27:20 +0300

    Configured the detail and list pages using generic class-based views

--- catalog/views.py
+++ catalog/views.py
@@ -16,12 +16,43 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Generating genre with name 'literature'
+
+    num_genre_literature = Genre.objects.filter(name__icontains='literature').count()
+
+    # Generating genre with name 'Fiction'
+    num_genre_fiction = Genre.objects.filter(name__icontains='fiction').count()
+
+
+    # Generating books with name 'the'
+    num_books_with_the = Book.objects.filter(title__icontains='the').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genre_literature': num_genre_literature,
+        'num_genre_fiction': num_genre_fiction,
+        'num_books_with_the': num_books_with_the
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
